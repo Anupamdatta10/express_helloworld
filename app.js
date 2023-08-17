5640a62a33c9101eaef122b2200a72e084d176f3
Anupam Datta <anupamdatta10@gmail.com>
2023-08-17 18:49:24 +0000

    docker file added

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
-const express = require('express')
+const express = require('express');
+const res = require('express/lib/response');
 const app = express()
-const port = 3000
+const port = 3001
+var mysql = require('mysql');
 
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root123",
+  port:3306
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
   res.render('index');
+})
+
+app.get('/hello',(req,res)=>{
+  res.send({"message":"hello world!!"})
 })
 
 app.listen(port, () => {
